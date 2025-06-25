1d005bdef8291fe26207a720c05f071bb3958da3
Alex Boucher <aboucher51@gmail.com>
2025-06-25 00:37:40 -0400

    added nix enemy setting

--- prototypes/planet/nix/nix_map_gen.lua
+++ prototypes/planet/nix/nix_map_gen.lua
@@ -1,67 +1,62 @@
-shipyard = function()
-    return
-    {
-      property_expression_names =
-      {
-        elevation = "vulcanus_elevation",
-        temperature = "vulcanus_temperature",
-        moisture = "vulcanus_moisture",
-        aux = "vulcanus_aux",
-        -- cliffiness = "cliffiness_basic",
-        -- cliff_elevation = "cliff_elevation_from_elevation",
-
-      },
-      autoplace_controls =
-      {
-        ["nix_copper_iron_ore"] = {},
-        ["nix_coal_stone_ore"] = {},
-        ["nix_crude_slush"] = {},
-        ["nix_dark_matter"] = {},
-        ["nix_enemy_base"] = {},
-        --["rocks"] = {}, -- can't add the rocks control otherwise nauvis rocks spawn
-      },
-      autoplace_settings =
-      {
-        ["tile"] =
-        {
-          settings =
-          {
-            ["snow-flat"] = {},
-            ["snow-crests"] = {},
-            ["snow-lumpy"] = {},
-            ["snow-patchy-grass"] = {},
-            ["ice-rough-nix"] = {},
-            ["ice-smooth-nix"] = {},
-          }
-        },
-        ["decorative"] =
-        {
-          settings =
-          {
-            ["carbon-iceberg-medium"] = {},
-            ["carbon-iceberg-small"] = {},
-            ["carbon-iceberg-tiny"] = {},
-            ["aqulio-ice-decal-blue"] = {},
-            ["aqulio-snowy-decal"] = {},
-            ["snow-drift-decal"] = {}
-          }
-        },
-        ["entity"] =
-        {
-          settings =
-          {
-            ["jellyfish-spawner"] = {},
-            ["dark-matter-vent"] = {},
-            ["crude-slush"] = {},
-            ["iron-copper-ore"] = {},
-            ["coal-stone-ore"] = {},
-            ["carbon-iceberg-huge"] = {},
-            ["carbon-iceberg-big"] = {}
-
-          }
-        }
-      }
+local function shipyard()
+    -- Build entity settings table
+    local entity_settings = {
+        ["dark-matter-vent"] = {},
+        ["crude-slush"] = {},
+        ["iron-copper-ore"] = {},
+        ["coal-stone-ore"] = {},
+        ["carbon-iceberg-huge"] = {},
+        ["carbon-iceberg-big"] = {}
     }
-  end
+
+    -- Conditionally add jellyfish-spawner
+    if settings.startup["is-nix-enemies-enabled"].value == true then
+        entity_settings["jellyfish-spawner"] = {}
+    end
+
+    return {
+        property_expression_names = {
+            elevation = "vulcanus_elevation",
+            temperature = "vulcanus_temperature",
+            moisture = "vulcanus_moisture",
+            aux = "vulcanus_aux",
+            -- cliffiness = "cliffiness_basic",
+            -- cliff_elevation = "cliff_elevation_from_elevation",
+        },
+        autoplace_controls = {
+            ["nix_copper_iron_ore"] = {},
+            ["nix_coal_stone_ore"] = {},
+            ["nix_crude_slush"] = {},
+            ["nix_dark_matter"] = {},
+            ["nix_enemy_base"] = {},
+            --["rocks"] = {}, -- can't add the rocks control otherwise nauvis rocks spawn
+        },
+        autoplace_settings = {
+            ["tile"] = {
+                settings = {
+                    ["snow-flat"] = {},
+                    ["snow-crests"] = {},
+                    ["snow-lumpy"] = {},
+                    ["snow-patchy-grass"] = {},
+                    ["ice-rough-nix"] = {},
+                    ["ice-smooth-nix"] = {},
+                }
+            },
+            ["decorative"] = {
+                settings = {
+                    ["carbon-iceberg-medium"] = {},
+                    ["carbon-iceberg-small"] = {},
+                    ["carbon-iceberg-tiny"] = {},
+                    ["aqulio-ice-decal-blue"] = {},
+                    ["aqulio-snowy-decal"] = {},
+                    ["snow-drift-decal"] = {}
+                }
+            },
+            ["entity"] = {
+                settings = entity_settings
+            }
+        }
+    }
+end
 
 return shipyard
--- settings.lua
+++ settings.lua
@@ -7,6 +7,12 @@ data:extend({
     },
     {
         type = "bool-setting",
+        name = "is-nix-enemies-enabled",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
         name = "is-micro-gravity-lab-all-science-enabled",
         setting_type = "startup",
         default_value = false
