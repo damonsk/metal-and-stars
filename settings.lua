edf471b2277702bb3008ebea81b62af9944e70c3
Alex Boucher <aboucher51@gmail.com>
2025-06-25 01:45:51 -0400

    ready for a rip

--- settings.lua
+++ settings.lua
@@ -7,12 +7,6 @@ data:extend({
     },
     {
         type = "bool-setting",
-        name = "is-nix-enemies-enabled",
-        setting_type = "startup",
-        default_value = true
-    },
-    {
-        type = "bool-setting",
         name = "is-micro-gravity-lab-all-science-enabled",
         setting_type = "startup",
         default_value = false
