916f58e18fd6d2ed0126507e845cd232afc672f7
damonsk <dskelly16@gmail.com>
2025-07-20 21:54:24 +0100

    move out of control stage and into data stage. Follow same pattern as planetLibTiers

--- prototypes/dependency-updates/compatibility-scripts.lua
+++ prototypes/dependency-updates/compatibility-scripts.lua
@@ -1,16 +1,3 @@
-local PlanetsLibTiers = require("__PlanetsLibTiers__.data")
-
--- Add new planet tiers
-PlanetsLibTiers.modded_tiers.planet["nix"] = 3
-PlanetsLibTiers.modded_tiers.planet["ringworld"] = 3
-PlanetsLibTiers.modded_tiers.planet["shipyard"] = 2
-
--- Add new space-location tiers
-PlanetsLibTiers.modded_tiers["space-location"]["calidus-senestella-gate-senestella"] = 1.1
-PlanetsLibTiers.modded_tiers["space-location"]["calidus-senestella-gate-calidus"] = 1.2
-PlanetsLibTiers.modded_tiers["space-location"]["mirandus"] = 4
-
-
 script.on_event(defines.events.on_built_entity, function(event)
     local entity = event.entity
     if not entity.valid then return end
